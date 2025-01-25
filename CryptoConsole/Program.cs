03788fbc1cfee5ebac88ff4e150500ab9e9d4386
Stephano <stephano@live.se>
2025-01-26 00:14:31 +0100

    add: initial setup

---
+++ CryptoConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CryptoConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
